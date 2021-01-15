0df7e029561456fd97c7edf90e4242ab63aa6a9d
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-01-15 15:53:23 +0200

    add tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,44 @@
-it('init test', () => {
-  expect(true).toBeTruthy();
+const fastify = require('fastify');
+const wrapFastify = require('../src/index.js');
+
+const methods = ['GET', 'POST', 'HEAD', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'];
+
+describe('fastify wrapper', () => {
+  let app;
+
+  beforeAll(() => {
+    const wrappedFastify = wrapFastify(fastify);
+    app = wrappedFastify();
+
+    methods.forEach((method) => {
+      app.route({
+        method,
+        url: '/',
+        handler: (req, reply) => {
+          reply.send({ method });
+        },
+      });
+    });
+  });
+
+  describe('#should be override', () => {
+    test.each([
+      ['POST', 'HEAD'],
+      ['POST', 'PUT'],
+      ['POST', 'DELETE'],
+      ['POST', 'OPTIONS'],
+      ['POST', 'PATCH'],
+    ])('#test from %s to %s', async (methodFrom, methodTo) => {
+      const res = await app.inject({
+        method: methodFrom,
+        url: `/?_method=${methodTo}`,
+      });
+
+      const actual = JSON.parse(res.body);
+      const expected = { method: methodTo };
+
+      expect(res.statusCode).toBe(200);
+      expect(actual).toEqual(expected);
+    });
+  });
 });
