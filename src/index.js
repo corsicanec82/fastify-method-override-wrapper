105ad0f75076fb0b2a6507e02e5d860082108a0a
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2021-01-15 17:28:46 +0200

    improve solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const parseurl = require('parseurl');
+
 const allowedMethods = ['HEAD', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'];
 
 const overrideMethod = (req) => {
@@ -5,9 +7,9 @@ const overrideMethod = (req) => {
   const originalMethod = req.method.toUpperCase();
 
   if (originalMethod === 'POST') {
-    const base = req.headers.origin;
-    const url = new URL(originalUrl, `http://${base}`);
-    const queryMethod = url.searchParams.get('_method');
+    const url = parseurl(req);
+    const searchParams = new URLSearchParams(url.query);
+    const queryMethod = searchParams.get('_method');
     const method = queryMethod && queryMethod.toUpperCase();
     if (method && allowedMethods.includes(method)) {
       req.method = method;
